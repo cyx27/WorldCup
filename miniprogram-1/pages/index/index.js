07fbdcbf4ba64bd3b30e0178855e4e1f70566b7f
Tianmasikong <210583478@qq.com>
2022-04-08 10:21:17 +0800

    实现了球员爬取并将服务器部署在远端，接口已写在index.js中

--- miniprogram-1/pages/index/index.js
+++ miniprogram-1/pages/index/index.js
@@ -1,20 +1,35 @@
 // index.js
+
+
 // 获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    player:[ ]
+    player:[ ],
+    todoname:'卡塔尔'
+  },
+  handleinput(e){
+    this.setData({
+      todoname:e.detail.value
+    })
   },
   handletap(e){
-    let that=this
-    wx.cloud.callFunction({
-      name: 'getData',
-      success: res => {
-        that.setData({
-          player: res.result
-        })
-      }
+    let that = this;
+    var url="http://150.158.20.204:8000";
+    wx.request({
+      url: url,
+      data:{
+       name:that.data.todoname
+      },
+      method:'get',
+      header: {'Content-Type': 'application/json'},
+      success: function(res) {
+        console.log(res.data);
+              that.setData({
+                player:res.data
+              })
+         }
     })
  }
 })
