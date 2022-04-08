6e6599f910fca89bdf5ea3da0a2a06cfcae0d6b6
cyx27 <1643034754@qq.com>
2022-04-08 15:11:43 +0800

    增加了国旗

--- miniprogram-1/pages/event_info/event_info.js
+++ miniprogram-1/pages/event_info/event_info.js
@@ -1,13 +1,53 @@
 Page({
 
   data: {
-    groupA:['卡塔尔','厄瓜多尔','塞内加尔','荷兰'],
-    groupB:['英格兰','伊朗','美国','威尔士'],
-    groupC:['阿根廷','沙特阿拉伯','墨西哥','波兰'],
-    groupD:['法国','秘鲁','丹麦','突尼斯'],
-    groupE:['西班牙','哥斯达黎加','德国','日本'],
-    groupF:['比利时','加拿大','摩洛哥','克罗地亚'],
-    groupG:['巴西','塞尔维亚','瑞士','喀麦隆'],
-    groupH:['葡萄牙','加纳','乌拉圭','韩国'],
+    groupA:[
+      ['卡塔尔', 'https://sd.qunliao.info/fastdfs3/M00/B5/7C/ChOxM1xC2PyAX4i0AAAJXV6GRFw977.png'],
+      ['厄瓜多尔', 'https://sd.qunliao.info/fastdfs3/M00/B5/75/ChOxM1xC2FSADy_DAAALaURezqo279.png'],
+      ['塞内加尔', 'https://sd.qunliao.info/fastdfs3/M00/B5/7D/ChOxM1xC2RKAS3NNAAAC2Nr8OzA389.png'],
+      ['荷兰', 'https://sd.qunliao.info/fastdfs3/M00/B5/7B/ChOxM1xC2N6AEWYMAAABPByVIVM465.png']
+    ],
+    groupB:[
+      ['英格兰', 'https://sd.qunliao.info/fastdfs3/M00/B5/76/ChOxM1xC2G2Acp31AAABNMvdP0U855.png'],
+      ['伊朗', 'https://sd.qunliao.info/fastdfs3/M00/B5/78/ChOxM1xC2KGAIsqKAAAVUXXXPL0755.png'],
+      ['美国', 'https://sd.qunliao.info/fastdfs3/M00/B5/7F/ChOxM1xC2UmAAa6sAAAQEZEKYAA997.png'],
+      ['威尔士', 'https://sd.qunliao.info/fastdfs3/M00/B5/80/ChOxM1xC2VGAYhOTAAAbZ1CFkQg735.png']
+    ],
+    groupC:[
+      ['阿根廷', 'https://sd.qunliao.info/fastdfs3/M00/B5/72/ChOxM1xC2A-AI_uOAAAHB-5pMAU501.png'],
+      ['沙特阿拉伯', 'https://sd.qunliao.info/fastdfs3/M00/B5/7D/ChOxM1xC2QmAWthoAAAKQAUuoQY168.png'],
+      ['墨西哥', 'https://sd.qunliao.info/fastdfs3/M00/B5/7A/ChOxM1xC2NOAIENZAAAIVenmXhE159.png'],
+      ['波兰', 'https://sd.qunliao.info/fastdfs3/M00/B5/7C/ChOxM1xC2PGALY-zAAAAqYWqc08697.png']
+    ],
+    groupD:[
+      ['法国', 'https://sd.qunliao.info/fastdfs3/M00/B5/77/ChOxM1xC2IKANG6tAAABFW9OqCQ523.png'],
+      ['秘鲁', 'https://sd.qunliao.info/fastdfs3/M00/B5/7B/ChOxM1xC2OyAMboLAAAE67UTWH4126.png'],
+      ['丹麦', 'https://sd.qunliao.info/fastdfs3/M00/B5/75/ChOxM1xC2FGAU4XgAAABUo4hnUQ558.png'],
+      ['突尼斯', 'https://sd.qunliao.info/fastdfs3/M00/B5/7F/ChOxM1xC2T6AVxyuAAAF5xZKmyM136.png']
+    ],
+    groupE:[
+      ['西班牙', 'https://sd.qunliao.info/fastdfs3/M00/B5/7E/ChOxM1xC2TCAWMemAAAJsy8Pgbg246.png'],
+      ['哥斯达黎加', 'https://sd.qunliao.info/fastdfs3/M00/B5/74/ChOxM1xC2EKAaFLCAAAGgA8WOkI815.png'],
+      ['德国', 'https://sd.qunliao.info/fastdfs3/M00/B5/77/ChOxM1xC2JGAd79VAAAAyGVvoVQ975.png'],
+      ['日本', 'https://sd.qunliao.info/fastdfs3/M00/B5/79/ChOxM1xC2L6AHvC4AAAEdawnP9Q689.png']
+    ],
+    groupF:[
+      ['比利时', 'https://sd.qunliao.info/fastdfs3/M00/B5/72/ChOxM1xC2CKAJTG3AAAAypTh1A8948.png'],
+      ['加拿大', 'https://sd.qunliao.info/fastdfs3/M00/B5/73/ChOxM1xC2DSAay2bAAAF1Vkv5Qg286.png'],
+      ['摩洛哥', 'https://sd.qunliao.info/fastdfs3/M00/B5/7A/ChOxM1xC2NWAZiM1AAADL6R9t6s156.png'],
+      ['克罗地亚', 'https://sd.qunliao.info/fastdfs3/M00/B5/74/ChOxM1xC2EOAbUHIAAAOg6JpgzM729.png']
+    ],
+    groupG:[
+      ['巴西', 'https://sd.qunliao.info/fastdfs3/M00/B5/73/ChOxM1xC2DCAM4slAAAMODYb5Wo093.png'],
+      ['塞尔维亚', 'https://sd.qunliao.info/fastdfs3/M00/B5/87/ChOxM1xC2iSAWoJLAAANi61d2EA772.png'],
+      ['瑞士', 'https://sd.qunliao.info/fastdfs3/M00/B5/7F/ChOxM1xC2TyANIAKAAABeUfO5gM520.png'],
+      ['喀麦隆', 'https://sd.qunliao.info/fastdfs3/M00/B5/73/ChOxM1xC2DOANFruAAAC0MdXvjY313.png']
+    ],
+    groupH:[
+      ['葡萄牙', 'https://sd.qunliao.info/fastdfs3/M00/B5/7C/ChOxM1xC2PuALvwRAAAK3F_koeE108.png'],
+      ['加纳', 'https://sd.qunliao.info/fastdfs3/M00/B5/77/ChOxM1xC2JGASs2QAAAEP6RcnSU543.png'],
+      ['乌拉圭', 'https://sd.qunliao.info/fastdfs3/M00/B5/80/ChOxM1xC2UyAFIQpAAAJFbiniWQ532.png'],
+      ['韩国', 'https://sd.qunliao.info/fastdfs3/M00/B5/7A/ChOxM1xC2MSAQaWAAAAL1VFSq5U098.png']
+    ],
   }
 })
