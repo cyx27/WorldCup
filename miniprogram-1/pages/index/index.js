40cbe5e6ca90d8571ff7740faaa3517da739f59f
Tianmasikong <210583478@qq.com>
2022-05-28 10:31:30 +0800

    增加了较丑的倒计时

--- miniprogram-1/pages/index/index.js
+++ miniprogram-1/pages/index/index.js
@@ -4,7 +4,12 @@
 const app = getApp()
 
 Page({
-  data: {
+    data: {  nowDate: '2022-11-21 18:00:00', //结束时间
+    countdown: '', //倒计时
+    days: '00', //天
+    hours: '00', //时
+    minutes: '00', //分
+    seconds: '00', //秒
     res: !1,
     imageWidth: wx.getSystemInfo().windowWidth,
     imageHeight: wx.getSystemInfo().screenHeight,
@@ -19,6 +24,39 @@ Page({
       todoname:e.detail.value
     })
   },
+  countTime() {
+    let days,hours, minutes, seconds;
+    let nowDate = this.data.nowDate;
+    console.log(nowDate)
+    let that = this;
+    let now = new Date().getTime();
+    let end = new Date(nowDate).getTime(); //设置截止时间
+    // console.log("开始时间：" + now, "截止时间:" + end);
+    let leftTime = end - now; //时间差                         
+    if (leftTime >= 0) {
+      days = Math.floor(leftTime / 1000 / 60 / 60 / 24);
+      hours = Math.floor(leftTime / 1000 / 60 / 60 % 24);
+      minutes = Math.floor(leftTime / 1000 / 60 % 60);
+      seconds = Math.floor(leftTime / 1000 % 60);
+      seconds = seconds < 10 ? "0" + seconds : seconds
+      minutes = minutes < 10 ? "0" + minutes : minutes
+      hours = hours < 10 ? "0" + hours : hours
+      that.setData({
+        countdown: days+":"+hours + "：" + minutes + "：" + seconds,
+        days,
+        hours,
+        minutes,
+        seconds
+      })
+      // console.log(that.data.countdown)
+      //递归每秒调用countTime方法，显示动态时间效果
+      setTimeout(that.countTime, 1000);
+    } else {
+      that.setData({
+        countdown: '世界杯已开赛!'
+      })
+    }
+ },
   handletap(e){
     let that = this;
     var url="https://segmenter.xyz:8000";
@@ -60,7 +98,9 @@ Page({
   //          }
   //     })
   // },
-
+  onLoad(){
+    this.countTime()
+  },
   onShow(){
     let that = this;
     var url="https://segmenter.xyz:8000";
